146e689168763532d58aa643b9e375700509274a
AnnaBertolani <bertolani.anna@gmail.com>
2019-02-02 17:33:50 +0100

    random spawn + destroy object - teleport

---
+++ Assets/script/PotionMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionMovement : MonoBehaviour
+{
+
+    public Transform TpLoc;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log(TpLoc.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Respawntrigger")
+        {
+            transform.position = new Vector3(TpLoc.transform.position.x, TpLoc.transform.position.y, TpLoc.transform.position.z);
+        }
+        //Debug.Log(TpLoc.transform.position);
+    }
+
+
+
+}
---
+++ Assets/script/RandomSpawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour {
+
+
+    public GameObject prefab1, prefab2, prefab3, prefab4, prefab5, prefab6, prefab7;
+    public float Spawnrate = 2f;
+    float NextSpawn = 0f;
+    int whatTospawn;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > NextSpawn)
+        {
+            whatTospawn = Random.Range(1, 8);                   //random value between 1 and 7 (8 ex)
+            switch (whatTospawn)
+            {
+                case 1:
+                    Instantiate(prefab1, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(prefab2, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(prefab3, transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(prefab4, transform.position, Quaternion.identity);
+                    break;
+              
+
+            }
+
+            NextSpawn = Time.time + Spawnrate;
+        }
+    }
+}
